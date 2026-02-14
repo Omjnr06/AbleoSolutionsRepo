3be2d5b45a2aa62df9e84deab42fed12e5ea5285
Omjnr06 <oj.magbadelo@gmail.com>
2026-02-14 01:45:57 -0500

    Fixed the repo structure, simplified to be able to be hosted on web, and ready for next phase with webhooks for clients and then eventually chatbase integration

--- merchant-onboarding-mvp/testcreds.js
+++
@@ -1,4 +0,0 @@
-const { generateClientId, generateClientSecret } = require("./credgenerator");
-
-console.log("Generated Client ID:", generateClientId());
-console.log("Generated Client Secret:", generateClientSecret());
